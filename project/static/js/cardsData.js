0b25fbe47424919f03c4e59a19f3b48d88adcc96
v0landem0rt <59765280+v0landem0rt@users.noreply.github.com>
2021-05-30 16:38:11 +0300

    changes

--- project/static/js/cardsData.js
+++ project/static/js/cardsData.js
@@ -64,10 +64,10 @@ let cardsData = [{
   avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
   user: {
     name: 'Okinami',
-    handle: 'twitterid',
-    title: 'Lead Developer'
+    handle: 'cryptocurrency info',
+    title: 'Transactions'
   },
-  content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
+  content: 'Here you can see your trasactions'
 },
 {
   id: 1,
